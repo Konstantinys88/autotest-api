0160ddf639edd1f9bef336605784c7133568225b
Konstantin <maschkovc@yandex.ru>
2025-12-07 17:52:17 +0300

    user crate/autentification/id

---
+++ httpx_create_user.py
@@ -0,0 +1,32 @@
+import httpx
+from tools.fakers import get_random_email
+
+# Создать пользователя
+user_payload = {
+  "email": get_random_email(),
+  "password": "admin",
+  "lastName": "Oleg",
+  "firstName": "Semen",
+  "middleName": "Egor"
+}
+
+response = httpx.post('http://localhost:8000/api/v1/users', json=user_payload)
+user_payload_dara = response.json()
+print(f'Status: {response.status_code}, Response: {user_payload_dara}')
+
+# Аутентификация
+login_payload = {
+    "email": f'{user_payload_dara['user']['email']}',
+    "password": "admin"
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print(f'Status: {login_response.status_code}, Login response: {login_response_data}')
+
+# Получение пользователя
+user_headers = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+user_id = user_payload_dara['user']['id']
+print(user_id)
+response = httpx.get(f'http://localhost:8000/api/v1/users/{user_id}', headers=user_headers)
+user_id_data = response.json()
+print(f'Status: {response.status_code}, Response: {user_id_data}')
---
+++ tools/fakers.py
@@ -0,0 +1,5 @@
+import time
+
+def get_random_email():
+    return f'test@example{time.time()}.ru'
+
